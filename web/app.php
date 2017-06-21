9e253ff94409ff7ce127f258d25651989c54a70f
Giorgio Sironi <g.sironi@elifesciences.org>
2017-06-21 13:34:55 +0000

    Introducing config file

--- web/app.php
+++ web/app.php
@@ -1,14 +1,48 @@
 <?php
+use Symfony\Component\HttpFoundation\Request;
 require_once __DIR__.'/../vendor/autoload.php';
+
+$config = include __DIR__.'/../config.php';
 
 $app = new Silex\Application();
 
-$app->get('/', function() use($app) {
-    return 'Hello World';
+$app->get('/', function() use($app, $config) {
+    $links = [];
+    
+    $links["Login with ORCID"] = "https://orcid.org/oauth/authorize?client_id={$config['orcid']['client_id']}&response_type=code&scope=/authenticate&redirect_uri={$config['base_url']}/oauth-url-back";
+    
+    $output = "<h1>Profiles demo</h1>\n";
+    foreach ($links as $label => $href) {
+        $output .= "<a href=\"$href\">$label</a>\n";
+    }
+
+    return $output;
+});
+
+$app->get('/oauth-url-back', function(Request $request) use ($app, $config) {
+    $code = $request->query->get('code');
+    $client = new GuzzleHttp\Client();
+    $response = $client->request('POST', 'https://orcid.org/oauth/token', [
+        'form_params' => [
+            'client_id' => $config['orcid']['client_id'],
+            'client_secret' => $config['orcid']['client_secret'],
+            'grant_type' => 'authorization_code',
+            'code' => $code,
+            'redirect_uri' => $config['base_url'],
+        ],
+        'headers' => [
+            'Accept' => 'application/json',
+        ],
+    ]);
+    return var_export(json_decode($response->getBody(), true), true);
 });
 
 $app->get('/ping', function() use($app) {
     return 'pong';
+});
+
+$app->error(function($exception) {
+    error_log($exception->getMessage()."\n".$exception->getFile().":".$exception->getLine());
 });
 
 $app->run();
