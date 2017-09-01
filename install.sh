c2a44e83676d0639a5b0287879036bc4e5b48c9d
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-09-01 13:45:23 +0100

    Force Python 3.6

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ set -e
 
 echo "[-] install.sh"
 
-if [ ! -e "venv/bin/python3" ]; then
-    echo "could not find venv/bin/python3, recreating venv"
+if [ ! -e "venv/bin/python3.6" ]; then
+    echo "could not find venv/bin/python3.6, recreating venv"
     rm -rf venv
-    virtualenv --python=python3 venv
+    virtualenv --python=python3.6 venv
 fi
 
 source venv/bin/activate
