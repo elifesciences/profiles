ad37a62b1b573d3fc49e8513ea28dc620032f866
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-09-06 09:32:18 +0100

    Update tests

--- test/test_oauth2.py
+++ test/test_oauth2.py
@@ -62,7 +62,7 @@ def test_it_redirects_when_authorizing(test_client: FlaskClient):
     assert response.status_code == 302
     assert response.headers['Location'] == 'http://www.example.com/server/authorize?' + urlencode(
         {'client_id': 'server_client_id', 'response_type': 'code', 'scope': '/authenticate',
-         'redirect_uri': 'http://localhost/oauth2/check',
+         'redirect_uri': 'https://localhost/oauth2/check',
          'state': dumps({'redirect_uri': 'http://www.example.com/client/redirect',
                          'client_id': 'client_id'})})
 
@@ -75,7 +75,7 @@ def test_it_redirects_with_the_original_state_when_authorizing(test_client: Flas
     assert response.status_code == 302
     assert response.headers['Location'] == 'http://www.example.com/server/authorize?' + urlencode(
         {'client_id': 'server_client_id', 'response_type': 'code', 'scope': '/authenticate',
-         'redirect_uri': 'http://localhost/oauth2/check',
+         'redirect_uri': 'https://localhost/oauth2/check',
          'state': dumps({'redirect_uri': 'http://www.example.com/client/redirect',
                          'client_id': 'client_id', 'original': 'foo'})})
 
