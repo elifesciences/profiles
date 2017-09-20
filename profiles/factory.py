081128a9a98a4b30f529125335edb13e6527a912
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-09-20 09:14:23 +0100

    Rely on UWSGI_SCHEME instead

--- profiles/factory.py
+++ profiles/factory.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_migrate import Migrate
-from werkzeug.contrib.fixers import ProxyFix
 
 from profiles.api import errors, oauth2, ping
 from profiles.clients import Clients
@@ -14,8 +13,6 @@ def create_app(config: Config, clients: Clients) -> Flask:
     app = Flask(__name__)
     app.TRAP_HTTP_EXCEPTIONS = True
     app.config.from_object(config)
-
-    app.wsgi_app = ProxyFix(app.wsgi_app)
 
     db.app = app
     db.init_app(app)
