a79deaa2c2f6075df452a1ceb67f3b2ddc458f15
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-09-25 09:46:12 +0100

    Don't rely on a model in a migration

--- migrations/versions/f17777978680_.py
+++ migrations/versions/f17777978680_.py
@@ -1,13 +1,21 @@
-from profiles.models import OrcidToken, db
+from alembic import op
+import sqlalchemy as sa
 
 revision = 'f17777978680'
 down_revision = '59a7cdca7cb0'
 branch_labels = None
 depends_on = None
 
+orcid_token_helper = sa.Table(
+    'orcid_token',
+    sa.MetaData(),
+)
+
 
 def upgrade():
-    db.session.query(OrcidToken).delete()
+    connection = op.get_bind()
+
+    connection.execute(orcid_token_helper.delete())
 
 
 def downgrade():
