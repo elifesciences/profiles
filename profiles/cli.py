0fa9a80d9df92fbc5bb1e44f4131ceb384d710e5
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-10-10 12:50:18 +0100

    Add types

--- profiles/cli.py
+++ profiles/cli.py
@@ -1,10 +1,12 @@
 from flask_script import Command
 
+from profiles.types import CanBeCleared
+
 
 class ClearCommand(Command):
     NAME = 'clear'
 
-    def __init__(self, *repositories):
+    def __init__(self, *repositories: CanBeCleared) -> None:
         super(ClearCommand, self).__init__()
         self.repositories = repositories
 
@@ -13,6 +15,6 @@ class ClearCommand(Command):
     # let's see whether we get more commands
     # and we keep flask_script in the long term
     # pylint: disable=method-hidden
-    def run(self):
+    def run(self) -> None:
         for each in self.repositories:
             each.clear()
