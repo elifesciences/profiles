e99318e480404847df2371e88d75f4d6d1f4d159
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-11-15 15:00:31 +0000

    Update the profile on a webhook request

--- profiles/api/webhook.py
+++ profiles/api/webhook.py
@@ -2,19 +2,27 @@ from flask import Blueprint
 from werkzeug.exceptions import NotFound
 from werkzeug.wrappers import Response
 
+from profiles.commands import update_profile_from_orcid_record
 from profiles.exceptions import ProfileNotFound
-from profiles.repositories import Profiles
+from profiles.orcid import OrcidClient
+from profiles.repositories import OrcidTokens, Profiles
 
 
-def create_blueprint(profiles: Profiles) -> Blueprint:
+def create_blueprint(profiles: Profiles, orcid_client: OrcidClient,
+                     orcid_tokens: OrcidTokens) -> Blueprint:
     blueprint = Blueprint('webhook', __name__)
 
     @blueprint.route('/orcid-webhook/<orcid>', methods=['POST'])
     def _update(orcid: str) -> Response:
         try:
-            profiles.get_by_orcid(orcid)
+            profile = profiles.get_by_orcid(orcid)
         except ProfileNotFound as exception:
             raise NotFound(str(exception)) from exception
+
+        access_token = orcid_tokens.get(profile.orcid)
+        orcid_record = orcid_client.get_record(orcid, access_token.access_token)
+
+        update_profile_from_orcid_record(profile, orcid_record)
 
         return Response(status=204)
 
--- test/test_webhook.py
+++ test/test_webhook.py
@@ -1,18 +1,30 @@
 from flask.testing import FlaskClient
+import requests_mock
 
 from profiles.database import db
-from profiles.models import Name, Profile
+from profiles.models import Name, OrcidToken, Profile
+from profiles.utilities import expires_at
 
 
-def test_it_returns_204_if_a_profile_is_found(test_client: FlaskClient) -> None:
+def test_it_updates_and_returns_204_if_a_profile_is_found(test_client: FlaskClient) -> None:
     profile = Profile('a1b2c3d4', Name('Foo Bar'), '0000-0002-1825-0097')
+    orcid_token = OrcidToken('0000-0002-1825-0097', 'access-token', expires_at(1234))
 
     db.session.add(profile)
+    db.session.add(orcid_token)
     db.session.commit()
 
-    response = test_client.post('/orcid-webhook/0000-0002-1825-0097')
+    with requests_mock.Mocker() as mocker:
+        mocker.get('http://www.example.com/api/v2.0/0000-0002-1825-0097/record',
+                   json={'person': {
+                       'name': {'family-name': {'value': 'Family Name'},
+                                'given-names': {'value': 'Given Names'}}
+                   }})
+
+        response = test_client.post('/orcid-webhook/0000-0002-1825-0097')
 
     assert response.status_code == 204
+    assert profile.name.preferred == 'Given Names Family Name'
 
 
 def test_it_returns_404_if_a_profile_is_not_found(test_client: FlaskClient) -> None:
