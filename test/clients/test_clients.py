b7e1eea88fd28d7635f8ce4d832c43083bcb8075
Sean Wiseman <30619751+seanwiseman@users.noreply.github.com>
2017-11-15 16:05:43 +0000

    Add property based tests to the test suite (#45)
    * Add property based tests to the client test suite
    
    * Changed database_file location via profile
    
    Removed helper function in place of explicit args
    
    * Also move Hypothesis home directory under build
    
    * Moved 'tomorrow' & 'yesterday' to @fixtures
    
    * Extended property based tests

--- test/clients/test_clients.py
+++ test/clients/test_clients.py
@@ -1,25 +1,32 @@
+from hypothesis import given
+from hypothesis.strategies import lists, text
+
 import pytest
 
 from profiles.clients import Client, Clients
 
 
-def test_it_contains_clients():
-    client1 = Client('name1', 'client_id1', 'client_secret1', 'redirect_uri1')
-    client2 = Client('name2', 'client_id2', 'client_secret2', 'redirect_uri2')
-    clients = Clients(client1, client2)
+@given(lists(text(), max_size=4, min_size=4), lists(text(), max_size=4, min_size=4))
+def test_it_contains_clients(client1_args, client2_args):
+    clients = Clients(Client(*client1_args), Client(*client2_args))
 
     assert len(clients) == 2
 
 
-def test_it_finds_clients():
-    client1 = Client('name1', 'client_id1', 'client_secret1', 'redirect_uri1')
-    client2 = Client('name2', 'client_id2', 'client_secret2', 'redirect_uri2')
+@given(text(min_size=1))
+def test_it_finds_clients(id_base):
+    client_id1 = id_base + '1'
+    client_id2 = id_base + '2'
+    unknown_client_id = id_base + 'unknown'
+
+    client1 = Client('name1', client_id1, 'client_secret1', 'redirect_uri1')
+    client2 = Client('name2', client_id2, 'client_secret2', 'redirect_uri2')
     clients = Clients(client1, client2)
 
     assert len(clients) == 2
 
-    assert clients.find('client_id1') == client1
-    assert clients.find('client_id2') == client2
+    assert clients.find(client_id1) == client1
+    assert clients.find(client_id2) == client2
 
     with pytest.raises(KeyError):
-        clients.find('client_id3')
+        clients.find(unknown_client_id)
