2bea6f5e6d475c1831a278d2b0711d3010095e93
Sean Wiseman <seanwiseman2012@gmail.com>
2017-11-23 09:32:24 +0000

    Updated log strings
    event_handler: additional typing for `changes` arg

--- profiles/events.py
+++ profiles/events.py
@@ -1,10 +1,11 @@
 import logging
-from typing import Callable
+from typing import Callable, List, Tuple
 
 from elife_bus_sdk.events import ProfileEvent
 from elife_bus_sdk.publishers import EventPublisher
 from flask import Flask
 
+from profiles.database import db
 from profiles.exceptions import UpdateEventFailure
 from profiles.models import Affiliation, EmailAddress, Profile
 
@@ -13,13 +14,13 @@ LOGGER = logging.getLogger(__name__)
 
 
 def send_update_events(publisher: EventPublisher) -> Callable[..., None]:
-    def event_handler(sender: Flask, changes: tuple) -> None:  # pylint:disable=unused-argument
+    def event_handler(sender: Flask, changes: List[Tuple[db.Model, str]]) -> None:  # pylint:disable=unused-argument
         ids = []
 
         LOGGER.info('Processing event(s)')
 
         for instance, operation in changes:  # pylint:disable=unused-variable
-            LOGGER.info('Event: %s - %s', instance, operation)
+            LOGGER.info('Found operation %s %s', instance, operation)
             if isinstance(instance, Profile):
                 ids.append(instance.id)
             if isinstance(instance, (Affiliation, EmailAddress)):
@@ -27,7 +28,7 @@ def send_update_events(publisher: EventPublisher) -> Callable[..., None]:
 
         for profile_id in set(ids):
             try:
-                LOGGER.info('Sending event for profile id: %s', profile_id)
+                LOGGER.info('Sending event for Profile %s', profile_id)
 
                 # send message to bus indicating a profile change
                 publisher.publish(ProfileEvent(id=profile_id))
