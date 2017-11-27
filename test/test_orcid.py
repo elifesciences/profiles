8cdfe79529a53ef02cf9c66da86c63e21faa1334
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-11-27 08:37:58 +0000

    Update ORCID API to 2.1

--- test/test_orcid.py
+++ test/test_orcid.py
@@ -9,7 +9,7 @@ def test_it_gets_a_record():
     orcid_client = OrcidClient('http://www.example.com/api')
 
     with requests_mock.Mocker() as mocker:
-        mocker.get('http://www.example.com/api/v2.0/0000-0002-1825-0097/record',
+        mocker.get('http://www.example.com/api/v2.1/0000-0002-1825-0097/record',
                    request_headers={'Accept': 'application/orcid+json',
                                     'Authorization': 'Bearer 1/fFAGRNJru1FTz70BzhT3Zg'},
                    json={'foo': 'bar'})
@@ -23,6 +23,6 @@ def test_it_raises_http_errors():
     orcid_client = OrcidClient('http://www.example.com/api')
 
     with requests_mock.Mocker() as mocker, pytest.raises(HTTPError):
-        mocker.get('http://www.example.com/api/v2.0/0000-0002-1825-0097/record', status_code=404)
+        mocker.get('http://www.example.com/api/v2.1/0000-0002-1825-0097/record', status_code=404)
 
         orcid_client.get_record('0000-0002-1825-0097', '1/fFAGRNJru1FTz70BzhT3Zg')
