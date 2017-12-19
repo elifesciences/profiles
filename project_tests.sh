e371dbdbd6eed0543dc30e06fcbd0601dceb3f35
Sean Wiseman <seanwiseman2012@gmail.com>
2017-12-19 14:55:13 +0000

    Add test coverage

--- project_tests.sh
+++ project_tests.sh
@@ -3,7 +3,10 @@ set -e
 
 source venv/bin/activate
 
+pip install coveralls
 pip install proofreader==0.0.2
 
 python -m proofreader manage.py profiles/ test/
-python -m pytest --junitxml=build/pytest.xml
+coverage run -m pytest --junitxml=build/pytest.xml
+
+COVERALLS_REPO_TOKEN=$(cat /etc/coveralls/tokens/profiles) coveralls`
