0e5f7efeff1777a72932306e552264dac5df3de7
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-15 17:05:00 +0000

    Acknowledge script is correctly executed on startup

--- config/application_user.sql
+++ config/application_user.sql
@@ -1,4 +1,3 @@
 -- TODO: compare with builder-base-formula scripts
--- doesn't get executed!
 CREATE USER app_user WITH PASSWORD 'app_password';
 GRANT ALL PRIVILEGES ON DATABASE profiles TO app_user;
