0a663d03fbc1355cace6858178b789dac691cbe7
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-15 10:04:56 +0000

    Extract requirements-debug.txt

--- project_tests.sh
+++ project_tests.sh
@@ -3,9 +3,6 @@ set -e
 
 source venv/bin/activate
 
-pip install coveralls
-pip install proofreader==0.0.2
-
 python -m proofreader manage.py profiles/ test/
 coverage run -m pytest --junitxml=build/pytest.xml
 
