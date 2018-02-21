443cddbf555a0af556d576902e4eb850ca3c4200
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-21 10:07:45 +0000

    Add smoke test for oauth first step

--- smoke_tests_wsgi.sh
+++ smoke_tests_wsgi.sh
@@ -6,3 +6,4 @@ host=${1:-profiles}
 wait_for_port 5432 15 db
 uwsgi_curl 127.0.0.1:9000 "${host}/ping"
 uwsgi_curl 127.0.0.1:9000 "${host}/profiles"
+[ "$(uwsgi_curl 127.0.0.1:9000 "${host}/oauth2/authorize?client_id=client_id&response_type=code" | grep 'HTTP/1.1' | tr -d '\n\r')" = 'HTTP/1.1 302 FOUND' ]
