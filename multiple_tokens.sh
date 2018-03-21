8a85294c1b8300d0d0775ca14c339727c8ff32c1
Giorgio Sironi <g.sironi@elifesciences.org>
2018-03-21 13:47:36 +0000

    Remove noisy clear output

--- multiple_tokens.sh
+++ multiple_tokens.sh
@@ -5,7 +5,7 @@ rm -f *.log
 docker-compose up -d --force-recreate
 while true; do
     echo "Clear data"
-    docker-compose run --rm wsgi venv/bin/python manage.py clear 2>/dev/null
+    docker-compose run --rm wsgi venv/bin/python manage.py clear >/dev/null
     echo "Two parallel profile creations"
     printf "a\nb\n" | xargs \
         -I {} \
