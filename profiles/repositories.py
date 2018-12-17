5e1f3eaee69c83f3da8501ebf96e9c4d95ee8d09
Giancarlo Fusiello <g.fusiello@elifesciences.org>
2018-12-17 15:39:12 +0000

    correct typo

--- profiles/repositories.py
+++ profiles/repositories.py
@@ -80,7 +80,7 @@ class SQLAlchemyOrcidTokens(OrcidTokens):
             orcid_token = self.db.session.query(OrcidToken).filter_by(orcid=orcid).one()
             self.db.session.delete(orcid_token)
         except NoResultFound:
-            msg = 'Unalbe to remove ORCID token for ORCID {}. Token not found'.format(orcid)
+            msg = 'Unable to remove ORCID token for ORCID {}. Token not found'.format(orcid)
             LOGGER.info(msg=msg)
             pass
 
