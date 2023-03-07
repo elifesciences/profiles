89435d16591507de5ca405c3da4c97c1ad517e32
Luke <lsh-0@users.noreply.github.com>
2023-03-07 14:26:00 +1030

    renames manage.py to app.py manage.py will become a thin wrapper around 'flask' this is because flask-script is dead and Flask now supports 'commands' but we have many references to 'python manage.py <something>' that need to be preserved.

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 import configparser
 import logging
 import os
-
-from flask_migrate import MigrateCommand
-from flask_script import Manager, Server, Shell
 import yaml
-
 from profiles.clients import Client, Clients
-from profiles.config import create_app_config
-from profiles.factory import create_app
-from profiles.logging import configure_logging
+import profiles.logging
+import profiles.config
+import profiles.factory
 
+# what is this doing?
 os.umask(int('002', 8))
 
 CONFIG_FILE = configparser.ConfigParser()
 CONFIG_FILE.read('app.cfg')
 
-CLIENTS_DATA = yaml.load(open('clients.yaml')) or {}
+# ---
+
+CLIENTS_DATA = yaml.load(open('clients.yaml'), Loader=yaml.FullLoader) or {}
 # remove deprecated configuration key
 for data in CLIENTS_DATA:
     # upgrade deprecated `redirect_uri` to `redirect_uris`
@@ -25,12 +24,22 @@ for data in CLIENTS_DATA:
         del CLIENTS_DATA[data]['redirect_uri']
 CLIENTS = Clients(*[Client(name, **CLIENTS_DATA[name]) for name in CLIENTS_DATA])
 
-CONFIG = create_app_config(CONFIG_FILE)
-configure_logging(env=CONFIG.name,
-                  level=getattr(logging, CONFIG.logging.get('level')),
-                  path=CONFIG.logging.get('path'))
+# ---
 
-APP = create_app(CONFIG, CLIENTS)
+CONFIG = profiles.config.create_app_config(CONFIG_FILE)
+profiles.logging.configure_logging(env=CONFIG.name,
+                                   level=getattr(logging, CONFIG.logging.get('level')),
+                                   path=CONFIG.logging.get('path'))
+
+APP = profiles.factory.create_app(CONFIG, CLIENTS)
+
+# two entry points here:
+# 1. `manage.APP` used by wsgi
+# 2. `python manage.py`, used by humans
+
+
+
+'''
 MANAGER = Manager(APP)
 
 
@@ -47,3 +56,7 @@ for c in APP.commands:
 
 if __name__ == '__main__':
     MANAGER.run()
+'''
+
+def runserver():
+    pass
