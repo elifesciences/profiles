0ef183879f6bd19a633730a63e2cdc5d3b5fa9ff
Luke <lsh-0@users.noreply.github.com>
2023-05-22 13:34:24 +0930

    typo

--- test/commands/test_update_profile_from_orcid_record.py
+++ test/commands/test_update_profile_from_orcid_record.py
@@ -224,6 +224,35 @@ def test_it_removes_affiliations():
 
     assert len(profile.affiliations) == 0
 
+def test_it_updates_affiliations():
+    profile = Profile('12345678', Name('Name'))
+    profile.add_affiliation(Affiliation('1', Address(countries.get('gb'), 'City 1'),
+                                        'Organisation 1', Date(2017)))
+    orcid_record = {'activities-summary': {
+        'employments': {'employment-summary': [
+            {'put-code': 1, 'department-name': 'Department 2',
+             'organization': {'name': 'Organisation 2',
+                              'address': {'city': 'City 2', 'region': 'Region 2', 'country': 'US'}},
+             'start-date': {'year': {'value': '2016'}, 'month': {'value': '12'},
+                            'day': {'value': '31'}},
+             'end-date': {'year': {'value': '2018'}, 'month': {'value': '02'},
+                          'day': {'value': '03'}},
+             'visibility': 'LIMIT'},
+        ]},
+    }}
+
+    update_profile_from_orcid_record(profile, orcid_record)
+
+    assert len(profile.affiliations) == 1
+    assert profile.affiliations[0].department == 'Department 2'
+    assert profile.affiliations[0].organisation == 'Organisation 2'
+    assert profile.affiliations[0].address.city == 'City 2'
+    assert profile.affiliations[0].address.region == 'Region 2'
+    assert profile.affiliations[0].address.country == countries.get('US')
+    assert profile.affiliations[0].starts == Date(2016, 12, 31)
+    assert profile.affiliations[0].ends == Date(2018, 2, 3)
+    assert profile.affiliations[0].restricted is True
+    assert profile.affiliations[0].position == 0
 
 def test_it_updates_affiliations_2(database, session):
     """somehow `profiles` is achieving this state here:
